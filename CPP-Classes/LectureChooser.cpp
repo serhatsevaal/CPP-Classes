a9fa0aaa715410b784b483ae85aaf7fbb96d5616
Serhat <37984665+serhatsevaal@users.noreply.github.com>
2023-03-08 11:12:59 +0100

    Update LectureChooser.cpp

--- CPP-Classes/LectureChooser.cpp
+++ CPP-Classes/LectureChooser.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
 
 void lecture1();
-void lecture2();
 
 bool LectureChooser(int i) {
 	switch (i) {
 	case 1:
 		lecture1();
-		return 1;
-	case 2:
-		lecture2();
 		return 1;
 	default:
 		std::cout << "Invalid Entry";
